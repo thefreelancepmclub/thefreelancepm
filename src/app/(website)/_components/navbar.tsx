4eec03e6b912edac1f77237b5f71ca1611ab2b23
Monir Hossain <monirhrabby.personal@gmail.com>
2025-04-25 21:06:48 +0600

    developed: sidebar

--- src/app/(website)/_components/navbar.tsx
+++ src/app/(website)/_components/navbar.tsx
@@ -1,13 +1,63 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+const navlinks = [
+  {
+    id: 1,
+    label: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    label: "Subscription",
+    href: "/subscription",
+  },
+  {
+    id: 3,
+    label: "Job Board",
+    href: "/job",
+  },
+  {
+    id: 4,
+    label: "Templates",
+    href: "/templates",
+  },
+  {
+    id: 5,
+    label: "Courses",
+    href: "/courses",
+  },
+  {
+    id: 6,
+    label: "Quizzes",
+    href: "/quizzes",
+  },
+  {
+    id: 7,
+    label: "Testimonial",
+    href: "/testmonial",
+  },
+  {
+    id: 8,
+    label: "Coaching",
+    href: "/coaching",
+  },
+  {
+    id: 9,
+    label: "Faq",
+    href: "/faq",
+  },
+];
+
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -31,9 +81,30 @@ export default function Navbar() {
       }`}
     >
       <div className="container mx-auto flex items-center justify-between px-4 ">
-        <Button variant="outline" size="icon">
-          <Menu className="h-6 w-6" />
-        </Button>
+        <Sheet open={open} onOpenChange={(v) => setOpen(v)}>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Menu className="h-6 w-6" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <div className="space-y-4 mt-10">
+              {navlinks.map(({ id, href, label }) => (
+                <Button
+                  variant="link"
+                  effect="hoverUnderline"
+                  asChild
+                  key={id}
+                  className="text-black"
+                >
+                  <Link href={href} className="w-full">
+                    {label}
+                  </Link>
+                </Button>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
 
         <Image src="/Logo.png" width={40} height={100} alt="logo" />
 
