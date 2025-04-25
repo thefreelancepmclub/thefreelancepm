f397988006d92af416ea7e94215fb7e8c3a54bd4
Monir Hossain <monirhrabby.personal@gmail.com>
2025-04-25 15:32:34 +0600

    added: sign up page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        protocol: "https",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
