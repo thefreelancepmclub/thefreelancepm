3927af44f7b60ceac166b747a9fd6a4e454d231e
Monir Hossain <monirhrabby.personal@gmail.com>
2025-04-28 09:04:30 +0600

    a

---
+++ src/app/(website)/coaching/page.tsx
@@ -0,0 +1,11 @@
+import CoachingSubmissionForm from "./_components/coaching-submission-form";
+
+const Page = () => {
+  return (
+    <div>
+      <CoachingSubmissionForm />
+    </div>
+  );
+};
+
+export default Page;
