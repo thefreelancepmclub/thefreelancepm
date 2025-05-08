8829a2a0bd294db07e2a9e3d8fb9c719959e88d6
Monir Hossain <monirhrabby.personal@gmail.com>
2025-05-08 10:19:19 +0600

    removed: nylas requiretion

--- src/action/coaching/create-coaching.ts
+++ src/action/coaching/create-coaching.ts
@@ -2,7 +2,6 @@
 
 import { auth } from "@/auth";
 import { tierModel } from "@/helper/subscription";
-import { nylas } from "@/lib/nylas";
 import { prisma } from "@/lib/prisma";
 import { stripe } from "@/lib/stripe";
 import { coachingSchema, CoachingSchemaType } from "@/schemas/coaching";
@@ -14,18 +13,6 @@ export type webhookFor = "subscription" | "coaching" | "template" | "course";
 export async function createCoaching(data: CoachingSchemaType) {
   const cu = await auth();
   if (!cu?.user) redirect("/login");
-
-  // Find admin to access Nylas calendar
-  const admin = await prisma.user.findFirst({
-    where: { role: "admin" },
-  });
-
-  if (!admin?.grantId) {
-    return {
-      success: false,
-      message: "Admin calendar credentials not found.",
-    };
-  }
 
   // Get current user with their subscriptions and previous coaching sessions
   const user = await prisma.user.findFirst({
@@ -67,33 +54,6 @@ export async function createCoaching(data: CoachingSchemaType) {
   const subscription = user.userSubscriptions[0];
   const tier =
     tierModel[subscription.subscriptionId as keyof typeof tierModel] || "pro";
-
-  // Convert date + time string to Luxon DateTime object (in UTC)
-  const startDateTime = DateTime.fromFormat(
-    `${date} ${time}`,
-    "yyyy-MM-dd h:mm a",
-    { zone: "utc" },
-  );
-  const endDateTime = startDateTime.plus({ minutes: 30 });
-
-  // Query admin's calendar to check for overlapping events
-  const events = await nylas.events.list({
-    identifier: admin.grantId,
-    queryParams: { calendarId: admin.grantEmail as string },
-  });
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const isBusy = events.data.some((event: any) => {
-    const eventStart = DateTime.fromSeconds(event.start_time, { zone: "utc" });
-    const eventEnd = DateTime.fromSeconds(event.end_time, { zone: "utc" });
-
-    // Return true if event overlaps with desired time slot
-    return startDateTime < eventEnd && endDateTime > eventStart;
-  });
-
-  if (isBusy) {
-    return { success: false, message: "This time slot is already booked." };
-  }
 
   // Determine if payment is required based on subscription tier & usage
   let requiresPayment = false;
--- src/app/dashboard/coaching/page.tsx
+++ src/app/dashboard/coaching/page.tsx
@@ -1,10 +1,5 @@
-import { Calendar } from "lucide-react";
-
 import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/prisma";
 import Image from "next/image";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import CoachingStats from "./_components/coaching-stats";
 import { CoachingTableContainer } from "./_components/coaching-table-container";
@@ -13,56 +8,31 @@ export default async function CoachingPage() {
   const cu = await auth();
 
   if (cu?.user.role !== "admin") redirect("/login");
-  const admin = await prisma.user.findFirst({
-    where: {
-      id: cu.user.id,
-    },
-    select: {
-      grantEmail: true,
-      grantId: true,
-    },
-  });
-  if (admin?.grantEmail || admin?.grantId) {
-    return (
-      <div className=" p-6 ">
-        <div className="mb-16 flex items-center justify-between ">
-          <div className="flex items-center gap-5">
-            <Image
-              src="/coaching.png"
-              width={500}
-              height={500}
-              alt="coaching"
-              className="w-8 h-8"
-            />
-            <h1 className="text-2xl font-bold">Coaching</h1>
-          </div>
-          {/* <Button variant="default" className="gap-1">
+
+  return (
+    <div className=" p-6 ">
+      <div className="mb-16 flex items-center justify-between ">
+        <div className="flex items-center gap-5">
+          <Image
+            src="/coaching.png"
+            width={500}
+            height={500}
+            alt="coaching"
+            className="w-8 h-8"
+          />
+          <h1 className="text-2xl font-bold">Coaching</h1>
+        </div>
+        {/* <Button variant="default" className="gap-1">
             <Download className="h-4 w-4" />
             Export
           </Button> */}
-        </div>
-
-        {/* Coaching Stats Component */}
-        <CoachingStats />
-
-        {/* Coaching Table Container Component */}
-        <CoachingTableContainer />
       </div>
-    );
-  }
-  return (
-    <div className="min-h-[600px] flex flex-col justify-center items-center gap-y-5">
-      <h1 className="text-primary text-3xl font-bold">Connect Your Calendar</h1>
-      <p className="max-w-2xl text-center text-muted-foreground">
-        Easily integrate your calendar to sync events, schedule meetings, and
-        stay organized. Click the button below to securely connect your calendar
-        via OAuth.
-      </p>
-      <Button>
-        <Link href="/api/auth/nylas" className="flex items-center gap-x-2">
-          <Calendar /> Connect Calendar
-        </Link>
-      </Button>
+
+      {/* Coaching Stats Component */}
+      <CoachingStats />
+
+      {/* Coaching Table Container Component */}
+      <CoachingTableContainer />
     </div>
   );
 }
