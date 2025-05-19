b4a0e47e2c3ce800481c14d86132ba2901e0877c
Monir Hossain <monirhrabby.personal@gmail.com>
2025-05-19 07:05:45 +0600

    developed: carousel for testmonial

--- src/app/(website)/testmonial/testomonial_card.tsx
+++ src/app/(website)/testmonial/testomonial_card.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { Card, CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
 import { Testmonial } from "@prisma/client";
+import { Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
 import SuccessStoriesCard from "../success-stories/_components/SuccessStoriesCard";
 
 interface Props {
@@ -15,24 +14,26 @@ interface Props {
 
 export default function TestimonialCard({ data }: Props) {
   return (
-    <div className="flex flex-col justify-center">
-      <Carousel className=" mb-4">
-        <CarouselContent>
-          {data.map((item, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="">
-                <Card className="p-0 shadow-none border border-[#004AAD] ">
-                  <CardContent className="flex aspect-square items-center justify-center p-0 h-auto">
-                    <SuccessStoriesCard data={item} />
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        {/* <CarouselPrevious />
-        <CarouselNext /> */}
-      </Carousel>
+    <div className="">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper pb-[100px]"
+      >
+        {data.map((test) => (
+          <SwiperSlide key={test.id} className="mb-10">
+            <SuccessStoriesCard data={test} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
