a3d77f3be5faceed97d8970c1f0305308c73c5c4
Monir Hossain <monirhrabby.personal@gmail.com>
2025-05-23 09:18:48 +0600

    fixed: callback issue

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import { SignUpForm } from "./_components/sign-up-form";
 
 export default function SignUpPage() {
@@ -31,17 +30,6 @@ export default function SignUpPage() {
 
           {/* Sign up form component */}
           <SignUpForm />
-
-          {/* Login link */}
-          <div className="text-center text-sm">
-            <span className="text-gray-600">Already have Account?</span>{" "}
-            <Link
-              href="/login"
-              className="font-medium text-orange-500 hover:text-orange-600"
-            >
-              Sign In Here
-            </Link>
-          </div>
         </div>
       </div>
     </div>
